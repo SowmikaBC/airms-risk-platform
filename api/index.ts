e90be38ec18b49a9671849b3b99d8609f206481e
Sowmika BC <sowmikabc@gmail.com>
2026-08-27 19:53:57 +0530

    feat: add api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 ﻿import express from "express";
 import cors from "cors";
-import { registerRoutes } from "../server/routes.js";
+import { registerRoutes } from "../server/routes";
 
 const app = express();
 app.use(cors());
